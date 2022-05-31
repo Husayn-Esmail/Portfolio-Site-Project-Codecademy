81f167ff86f54b265216b285fa15d0d0fb4555d7
Husayn-Esmail <husayn@esmail.ca>
2022-05-30 21:24:39 -0600

    generated divs containing h2 based on secondary navigation

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+let secondary_nav = ['Python', 'C', 'Swift', 'HTML/CSS'];
+
+// document.body.createElement('nav');
+// let navs = document.body.getElementsByTagName('nav');
+// navs[0].appendChild('ul');
+
+const nav = document.getElementById('secondary').firstElementChild;
+for (let i = 0; i < nav.children.length; i++) {
+    // the elements within seocondary navigation
+    console.log(nav.children[i].innerHTML);
+    let div = document.createElement('div'); // creates a div to contain each image carousel
+    let newNode = document.createElement('h2'); // creates the title element for the section 
+    newNode.innerHTML = nav.children[i].innerHTML; // populates the title element 
+    div.appendChild(newNode); // adds the title to the div
+    document.body.appendChild(div); // inserts the div in the body
+}
+
+
+// let navi = document.getElementById('secondary');
+// console.log(navi.innerHTML);
+// console.log(navi);
