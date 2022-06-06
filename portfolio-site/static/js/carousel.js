cb81650b3b49d1cde7aa4d9693befd87bb9e25b3
Husayn-Esmail <husayn@esmail.ca>
2022-06-05 21:54:08 -0600

    created a class and some of the methods for carousel

--- portfolio-site/static/js/carousel.js
+++ portfolio-site/static/js/carousel.js
@@ -1,6 +1,60 @@
 // debated creating the secondary nav with js as well. 
 // let secondary_nav = ['Python', 'C', 'Swift', 'HTML/CSS'];
 
+class ProjectShowcase {
+    constructor(name, prog_language) {
+        this._name = name;
+        this._prog_language = prog_language;
+        this._images = [];
+        this._description = "";
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get prog_language() {
+        return this._prog_language;
+    }
+    
+    get images() {
+        return this._images;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set images(list_of_images) {
+        this._images = list_of_images;
+    }
+
+    set description(text) {
+        this._description = text;
+    }
+
+    addImageToList(image) {
+        this._images.push(image);
+    }
+
+    displayHeading() {
+        let div = document.createElement('div');
+        let newNode = document.createElement('h2');
+        newNode.innerHTML = this._name;
+        div.appendChild(newNode)
+    }
+
+    displayImages() {
+        let div = document.createElement('div');
+        for (let i = 0; i < this._images.length; i++) {
+            let newImage = document.createElement('img');
+            newImage.src = this._images[i];
+            div.appendChild(newImage);
+        }
+    }
+}
+
+
 
 const nav = document.getElementById('secondary').firstElementChild;
 for (let i = 0; i < nav.children.length; i++) {
