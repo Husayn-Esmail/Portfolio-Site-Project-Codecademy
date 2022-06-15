174a07a139f12fb3dc911e2d61ce72c9a13c8919
Husayn-Esmail <husayn@esmail.ca>
2022-06-14 22:40:06 -0600

    further developed js test cases

--- tests/test_carousel.js
+++ tests/test_carousel.js
@@ -3,20 +3,27 @@ const { Project, Technology } = require("../portfolio-site/static/js/carousel");
 
 describe("Test Project Class", () => {
     it('gets name', () => {
-        expected = 'name'
-        my_project = new Project('name', []);
-        actual = my_project.name;
+        const expected = 'name'
+        let my_project = new Project(expected, []);
+        const actual = my_project.name;
         assert.equal(expected, actual)
     });
     
     it('sets name', () => {
-        new_name = "new_name";
-        my_project = new Project('name', []);
+        const new_name = "new_name";
+        let my_project = new Project('name', []);
         my_project.setName = new_name;
-        expected = new_name;
-        actual = my_project.name;
+        const expected = new_name;
+        const actual = my_project.name;
         assert.strictEqual(actual, expected);
     })
 
+    it('gets images', () => {
+        const images = ['one', 'two'];
+        let my_project = new Project('name', images);
+        const actual = my_project.images;
+        const expected = images;
+        assert.strictEqual(expected, actual);
+    });
     
 });
