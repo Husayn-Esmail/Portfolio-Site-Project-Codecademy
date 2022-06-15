e7b730c0b70e32236c6ff63498e53f546d84440b
Husayn-Esmail <husayn@esmail.ca>
2022-06-14 20:57:42 -0600

    created a folder to hold the misc files that have been created and cleared setup.py

--- misc_files/set_path.sh
+++ misc_files/set_path.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# for working with non vscode editors.
 # run with either . ./set_path.sh or source ./set_path.sh
 # is only required for running standalone.
 export PYTHONPATH="$PWD"
--- setup.py
+++
@@ -1,4 +0,0 @@
-from recursive.recursive_indexer import sll
-
-if __name__ == '__main__':
-    print("I work")
