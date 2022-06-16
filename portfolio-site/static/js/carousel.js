e3e8d18989e17f7488b3fd3dfda580a1fdc47c79
Husayn-Esmail <husayn@esmail.ca>
2022-06-16 15:07:30 -0600

    able to display all content for carousel, fixed bug where get project description returned images

--- portfolio-site/static/js/carousel.js
+++ portfolio-site/static/js/carousel.js
@@ -2,94 +2,12 @@
 // let secondary_nav = ['Python', 'C', 'Swift', 'HTML/CSS'];
 
 
-
-// function myFunc(vars) {
-//     console.log(vars);
-//     let x = document.createElement('p');
-//     x.innerHTML = vars;
-//     document.body.appendChild(x);
-//     return vars;
-// }
-
-// class ProjectShowcase {
-//     constructor(prog_language) {
-//         // this._name = name;
-//         this._prog_language = prog_language;
-//         this._images = [];
-//         this._description = "";
-//     }
-
-//     // get name() {
-//     //     return this._name;
-//     // }
-
-//     get prog_language() {
-//         return this._prog_language;
-//     }
-    
-//     get images() {
-//         return this._images;
-//     }
-
-//     get description() {
-//         return this._description;
-//     }
-
-//     set images(list_of_images) {
-//         this._images = list_of_images;
-//     }
-
-//     set description(text) {
-//         this._description = text;
-//     }
-
-//     addImageToList(image) {
-//         this._images.push(image);
-//     }
-
-//     displayHeading(div) {
-//         let newHeading = document.createElement('h2');
-//         newHeading.innerHTML = this.prog_language;
-//         div.appendChild(newHeading)
-//     }
-
-//     displayImages(div) {
-//         let subdiv = document.createElement('div');
-//         for (let i = 0; i < this._images.length; i++) {
-//             let newImage = document.createElement('img');
-//             newImage.src = this._images[i];
-//             subdiv.appendChild(newImage);
-//             div.appendChild(subdiv); // add subdiv to parent div
-//         }
-//     }
-
-//     #populateNavButton(button, content, div) {
-//         // FIXME: returns the same list of images every time.
-//         button.innerHTML = content;
-//         button.className = "nav-button";
-//         // let img = document.createElement('img');
-//         let i = Math.floor(Math.random() * this.images.length);
-//         // img.src = images[i];
-//         console.log(i);
-//         button.addEventListener('click', (event) => {
-//             if (i === this.images.length){
-//                 i = 0;
-//             }
-//             console.log(this.images[i++]);
-//             // img.src = images[i++];
-//         });
-//         // div.appendChild(img);
-//     }
-//     createNavButtons(div) {
-//         let left = document.createElement('p');
-//         let right = document.createElement('p');
-//         this.#populateNavButton(left, '<', div);
-//         this.#populateNavButton(right, '>', div);
-//         div.appendChild(left);
-//         div.appendChild(right);
-//     }
-// }
-
+/*
+This is used for creating the div sections based on secondary navigation.
+We do not necessarily want to do this. It would be more beneificial to 
+create the divs based on the Technology object name.
+I'm keeping this here for reference though.
+*/
 // const nav = document.getElementById('secondary').firstElementChild;
 // for (let i = 0; i < nav.children.length; i++) {
 //     // the elements within secondary navigation
@@ -140,7 +58,7 @@ class Project {
     }
 
     get description() {
-        return this.__images;
+        return this.__description;
     }
 
     set name(newName) {
@@ -185,17 +103,21 @@ class Technology {
 
 class DisplayTechnology {
     constructor(technologyObject, pathToImages) {        
-        this.technology = technologyObject;
+        this._technology = technologyObject;
         this.path = pathToImages;
-        this.projects = technologyObject.projects;
+        this._projects = technologyObject.projects;
     }
 
     get technology() {
-        return this.technology;
+        return this._technology;
+    }
+
+    get projects() {
+        return this._projects;
     }
 
     set technology(new_technology) {
-        this.technology = new_technology;
+        this._technology = new_technology;
     }
 
     displayHeading(div) {
@@ -205,9 +127,9 @@ class DisplayTechnology {
          * the technology) to the div passed. returns null
          */
         let newHeading = document.createElement('h2');
-        newHeading.innerHTML = this.technology.prog_language;
+        newHeading.innerHTML = this._technology.prog_language;
         // need to set css classes or ids
-        newHeading.id = this.technology.prog_language;
+        newHeading.id = this._technology.prog_language;
         // add newHeading to div
         div.appendChild(newHeading);
         return null
@@ -236,9 +158,9 @@ class DisplayTechnology {
         const images = project.images;
         const project_name = project.name;
         // iterate through images and create img elements
-        for (image_name in images) {
+        for (let i = 0; i < images.length; i++) {
             const new_img = document.createElement('img');
-            const image_path = `${this.path}/${project_name}/${image_name}`;
+            const image_path = `${this.path}/${project_name}/${images[i]}`;
             new_img.src = image_path;
             new_img.className = 'project-image';
             subdiv.appendChild(new_img);
@@ -278,6 +200,18 @@ class DisplayTechnology {
     // create a function that processes the projects in this technology.
     // Call helper functions to display/get the resources to display such
     // elements.
+    displayProjects(projects, div) {
+        for (let i = 0; i < projects.length; i++) {
+            const project = projects[i];
+            const subheading = this.createSubheadingElement(project);
+            const images = this.createImageElements(project);
+            const description = this.createDescriptionElement(project);
+            div.appendChild(subheading);
+            div.appendChild(images);
+            div.appendChild(description);
+        }
+    }
+
 }
 
 
@@ -311,10 +245,29 @@ function pyObjectToJsObject(parsed_projects) {
         }
         technologies.push(newTech);
     }
-    console.log(technologies);
+    // console.log(technologies);
     return technologies;
 }
 
 let parsed_objects = parseItemsInList(projects_from_server)
 let technologies = pyObjectToJsObject(parsed_objects)
+
+// div encapsulating the carousel
+const main_div = document.createElement('div');
+
+//  iterate through technologies to generate the html
+let displayObjects = [];
+const basePath = "static/img/"
+for (let i = 0; i < technologies.length; i++) {
+    const path_to_images = basePath + technologies[i].prog_language;
+    const displayTech = new DisplayTechnology(technologies[i], path_to_images);
+    displayTech.displayHeading(main_div);
+    const projects = technologies[i].projects;
+    displayTech.displayProjects(projects, main_div);
+    displayTech.displayNavButtons(main_div);
+
+}
+
+document.body.appendChild(main_div);
+
 // export { Project, Technology };
