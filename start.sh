0bbb258ee38ded617f5d548dd07c01e94b55d9cf
Husayn-Esmail <husayn@esmail.ca>
2022-06-17 17:54:44 -0600

    fixed nav to top and styled landing page

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # setup for the server
-# export FLASK_APP=portfolio-site
-# export FLASK_ENV=development
-# export FLASK_RUN_PORT=5500
-# export FLASK_DEBUG=True
-# export FLASK_RUN_EXTRA_FILES="./static/css/"
-# export FLASK_RUN_HOST=0.0.0.0
-# export TEMPLATES_AUTO_RELOAD=True
-# echo $FLASK_RUN_EXTRA_FILES
-# flask run
-python3 portfolio-site/__init__.py
+export FLASK_APP=portfolio-site
+export FLASK_ENV=development
+export FLASK_RUN_PORT=5500
+export FLASK_DEBUG=True
+export FLASK_RUN_EXTRA_FILES="./static/css/"
+export FLASK_RUN_HOST=0.0.0.0
+export TEMPLATES_AUTO_RELOAD=True
+echo $FLASK_RUN_EXTRA_FILES
+flask run
+# python3 portfolio-site/__init__.py
