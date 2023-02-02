2291814d0d2105c569ac2d6b767e592a5149acdf
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-02-02 15:51:57 -0700

    figured out that js was causing overflow breaking footer

--- portfolio_site/static/js/footer.js
+++ portfolio_site/static/js/footer.js
@@ -1,20 +1,19 @@
 // const footer = document.createElement('footer');
 // footer.className = "footer";
-// const div = document.createElement('div');
+// // const div = document.createElement('div');
 
 // footer.appendChild(div);
 // document.getElementsByTagName('body').appendAfter = footer;
-
-const footer = document.createElement("footer");
-footer.className = "footer";
-const div = document.createElement('div')
-const p1 = document.createElement('p')
-p1.textContent = "Copyright 2022";
-const p2 = document.createElement('p')
-p2.textContent = "Husayn Esmail";
-div.className = "foot-content-container";
-div.appendChild(p1);
-div.appendChild(p2);
-footer.appendChild(div)
-document.getElementsByTagName('html')[0].append(footer)
+// const footer = document.createElement("footer");
+// footer.className = "footer";
+// const div = document.createElement('div')
+// const p1 = document.createElement('p')
+// p1.textContent = "Copyright 2022";
+// const p2 = document.createElement('p')
+// p2.textContent = "Husayn Esmail";
+// div.className = "foot-content-container";
+// div.appendChild(p1);
+// div.appendChild(p2);
+// footer.appendChild(div)
+// document.getElementsByTagName('html')[0].append(footer)
 
