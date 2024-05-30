35448492226d705964247527495c586e859b3b39
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2024-05-30 15:56:23 -0600

    modified to put a temporary template over my actual site for quick deployment

--- portfolio_site/original__init__.py
+++ portfolio_site/original__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from .recursive import recursive_indexer
 
 
 def create_app(config_filename=None):
@@ -17,7 +18,17 @@ def create_app(config_filename=None):
 
     @app.route('/', methods=['GET'])
     def index():
-        return render_template('iPortfolio/index.html')
+        image_path = "portfolio_site/static/img"
+        desc_path = "portfolio_site/static/descriptions"
+        ignored = ['favicons', 'hero', '.DS_Store']
+        projects = recursive_indexer.get_projects_for_display(image_path, desc_path, ignored)
+        # the projects out of read_images are not json serializable, must be
+        # converted first.
+        json_projects = []
+        for project in projects:
+            json_projects.append(project.to_json())
+        data = json_projects
+        return render_template('index.html', data=data)
 
     @app.route('/contact', methods=['GET', 'POST'])
     def contact():
